8d7627e02959e41eb957fd41475fedaa019f10aa
rymph <rymph0501@gmail.com>
2026-05-21 13:03:13 +0900

    chore: default settings done

---
+++ src/main/java/com/factory/anomaly/exception/AnomalyErrorCode.java
@@ -0,0 +1,32 @@
+package com.factory.anomaly.exception;
+
+import com.factory.common.core.exception.ErrorCode;
+
+public enum AnomalyErrorCode implements ErrorCode {
+    ;
+
+    private final int status;
+    private final String code;
+    private final String message;
+
+    AnomalyErrorCode(int status, String code, String message) {
+        this.status = status;
+        this.code = code;
+        this.message = message;
+    }
+
+    @Override
+    public int getStatus() {
+        return this.status;
+    }
+
+    @Override
+    public String getCode() {
+        return this.code;
+    }
+
+    @Override
+    public String getMessage() {
+        return this.message;
+    }
+}
---
+++ src/main/java/com/factory/anomaly/exception/AnomalyException.java
@@ -0,0 +1,16 @@
+package com.factory.anomaly.exception;
+
+import com.factory.common.core.exception.BaseException;
+import com.factory.common.core.exception.ErrorCode;
+
+public class AnomalyException extends BaseException {
+
+    public AnomalyException(ErrorCode errorCode) {
+        super(errorCode);
+    }
+
+    public AnomalyException(ErrorCode errorCode, Throwable cause) {
+        super(errorCode, cause);
+    }
+
+}
